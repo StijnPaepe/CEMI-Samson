49aeffe0969fce7bba3b51081c42ceda1c714b93
Kiana Truyens <71650707+kianatrs@users.noreply.github.com>
2021-03-31 13:03:03 +0200

    arthur-kiana_story11

--- src/domain/Vorm.java
+++ src/domain/Vorm.java
@@ -1,7 +1,22 @@
 package domain;
 
+import javafx.scene.paint.Color;
+
 public abstract class Vorm {
+    private Color kleur = Color.WHITE;
+
+    public Color getKleur() {
+        return kleur;
+    }
+
+    public void setKleur(Color kleur) {
+        if (kleur == null) throw new DomainException("kleur is null");
+        this.kleur = kleur;
+    }
 
     public abstract Omhullende getOmhullende();
 
+    public String toString() {
+        return "\n" + getOmhullende().toString();
+    };
 }
