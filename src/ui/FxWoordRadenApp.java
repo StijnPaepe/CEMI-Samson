2e931e97203fa852deb2ffb6374e0ebf8d5207b8
Stijn Paepe <stijn.paepe@student.ucll.be>
2021-04-01 14:47:10 +0200

    Jari_Stijn_Story12_UitvoerNogAfwerken

--- src/ui/FxWoordRadenApp.java
+++ src/ui/FxWoordRadenApp.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class FxWoordRadenApp extends Application {
